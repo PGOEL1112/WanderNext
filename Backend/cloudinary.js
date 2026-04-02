30dd9ee33c7fd04668de67cb93bf6489d4350246
PGOEL1112 <priyanshu.11goel@gmail.com>
2026-04-02 23:54:17 +0530

    new changes

--- Backend/cloudinary.js
+++ Backend/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinaryModule = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinaryModule.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_KEY_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
